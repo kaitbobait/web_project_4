22617a73f8088ac045809f40620421f655b86698
kaithill <k.m.hiller@gmail.com>
2020-11-10 17:20:00 -0500

    stylize popup, add x for close button

--- script.js
+++ script.js
@@ -1,28 +1,29 @@
+/*page section*/
+// let pageOverlay = document.querySelector('.page_overlay');
 
 /*profile section*/
 let editButton = document.querySelector('.profile__edit'); /*edit profile button*/
 let profileName = document.querySelector('.profile__name'); /*profile name */
 let profileTitle = document.querySelector('.profile__title'); /*profile description */
 
+/* form section */
 let buttonClose = document.querySelector('.popup__close-button'); /*close button for the popup*/
-let popupOpen = document.querySelector('.popup_open'); /*popup is visable*/
-let popupClose = document.querySelector('.popup'); /*popup has display: none*/
+let popup = document.querySelector('.popup'); /*popup has display: none*/
 let newName = document.querySelector('.input__name'); /*name written in the name input */
 let newTitle = document.querySelector('.input__title'); /*title written in the title input */
 let saveButton = document.querySelector('.popup__save-button'); /* save button */
 
 document.onload = closePopup();
 
-/*closes popup box*/
-function closePopup() {
-  popupOpen.classList.remove("popup_open");
-  popupClose.classList.add("popup");
-}
-
 /*opens popup box*/
 function openPopup() {
-  popupOpen.classList.add('popup_open');
-  popupClose.classList.remove('popupClose');
+  popup.classList.add('popup_open');
+  //pageOverlay.style.display = "block";
+}
+
+/*closes popup box*/
+function closePopup() {
+  popup.classList.remove("popup_open");
 }
 
 /*when clicks, the popup box closes */
@@ -30,18 +31,13 @@ buttonClose.addEventListener('click', closePopup);
 /*when clicks, the popup box opens */
 editButton.addEventListener('click', openPopup);
 
-function updateName() {
+/*updates the name and title input field, closes the popup, and prevents the default response from browser */
+function formSubmit(evt) {
   profileName.innerText = newName.value;
-}
-
-function updateTitle() {
   profileTitle.innerText = newTitle.value;
-}
-
-function updateInfo() {
-  updateName();
-  updateTitle();
   closePopup();
+  evt.preventDefault(); //stops browser from stubmitting the form in the default way
 }
 
-saveButton.addEventListener('click', updateInfo);
+/*when click the save button, runs the formSubmit function */
+saveButton.addEventListener('click', formSubmit);
