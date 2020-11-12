dd9aba9985ed0f770d3c3716a06118779e095ba1
kaithill <k.m.hiller@gmail.com>
2020-11-12 15:39:58 -0500

    feature: popup - allow form to display current name and title

--- script.js
+++ script.js
@@ -5,22 +5,25 @@ let profileName = document.querySelector('.profile__name'); /*profile name */
 let profileTitle = document.querySelector('.profile__title'); /*profile description */
 
 /* form section */
+let form = document.querySelector('.popup__form');
 let buttonClose = document.querySelector('.popup__close-button'); /*close button for the popup*/
 let popup = document.querySelector('.popup'); /*popup has display: none*/
 let newName = document.querySelector('.popup__input_text_name'); /*name written in the name input */
 let newTitle = document.querySelector('.popup__input_text_title'); /*title written in the title input */
 let saveButton = document.querySelector('.popup__save-button'); /* save button */
 
-document.onload = closePopup();
-
 /*opens popup box*/
 function openPopup() {
   popup.classList.add('popup_open');
+  newName.value = profileName.textContent;
+  newTitle.value = profileTitle.textContent;
+  
 }
 
 /*closes popup box*/
 function closePopup() {
   popup.classList.remove("popup_open");
+
 }
 
 /*when clicks, the popup box closes */
@@ -30,11 +33,11 @@ editButton.addEventListener('click', openPopup);
 
 /*updates the name and title input field, closes the popup, and prevents the default response from browser */
 function formSubmit(evt) {
-  profileName.innerText = newName.value;
-  profileTitle.innerText = newTitle.value;
+  profileName.textContent = newName.value;
+  profileTitle.textContent = newTitle.value;
   closePopup();
   evt.preventDefault(); //stops browser from stubmitting the form in the default way
 }
 
 /*when click the save button, runs the formSubmit function */
-saveButton.addEventListener('click', formSubmit);
+form.addEventListener('submit', formSubmit);
