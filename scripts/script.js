71d7eb497ddaf5bc69252001523a772ab0a786b7
kaithill <k.m.hiller@gmail.com>
2020-11-27 23:20:35 -0500

    add createPlaceTemplate function outside of addPlace

--- scripts/script.js
+++ scripts/script.js
@@ -29,7 +29,7 @@ const popupTitle = document.querySelector('.popup__image-title');
 const imagesExit = document.querySelector('.popup__close-button_images');
 
 /* determines which popup to open */
-function openPopup(evt) {
+const openPopup = (evt) => {
   if (evt.target === editButton) {
     popupEditProfile.classList.add('popup_open');
     newName.value = newName.textContent;
@@ -40,7 +40,7 @@ function openPopup(evt) {
 };
 
 /*closes either popup box if one is open*/
-function closePopup() {
+const closePopup = () => {
   if (popupEditProfile.classList.contains('popup_open')) {
     popupEditProfile.classList.remove("popup_open");
   } else if (popupEditPlaces.classList.contains('popup_open')) {
@@ -61,21 +61,11 @@ addPlacesExit.addEventListener('click', closePopup);
 // on click of close button, the popup image will disappear
 imagesExit.addEventListener('click', closePopup);
 
-/*updates the name and title input field, closes the popup, and prevents the default response from browser */
-function formSubmitEdit(evt) {
-  profileName.textContent = newName.value;
-  profileTitle.textContent = newTitle.value;
-  closePopup();
-  evt.preventDefault(); //stops browser from submitting the form in the default way (refreshes whenever you submit)
-}
-
-/*when click the save button, runs the formSubmit function */
-popupForms[0].addEventListener('submit', formSubmitEdit);
 
 /* PLACE CARD SECTION */
 
-/* Adds the 6 cards on load */
-function addPlace(place) {
+// creates and returns a template for the place cards
+const createPlaceTemplate = (place) =>  {
   // place card template
   const placeTemplate = document.querySelector('#place-template').content;
   // clone content of place card template tag
@@ -91,36 +81,57 @@ function addPlace(place) {
   // like button
   const heartButton = placeElement.querySelector('.places__heart-button');
   // When the heart button is clicked, a new class with a "liked" heart, appears
-  heartButton.addEventListener('click', function(place) {
-      heartButton.classList.toggle('places__heart-button_active');
-  });
+  const likeButton = place => {
+    heartButton.classList.toggle('places__heart-button_active');
+  };
+  // toggles like button when clicked
+  heartButton.addEventListener('click', likeButton); 
 
-  // opens a large popup of current place card image
-  function picturePopup () {
+  // image popup delete button
+  const deleteButton = placeElement.querySelector('.places__delete-button');
+
+  // removes the selected place card 
+  const deletePlace = (evt) => {
+    evt.target.closest('.places__item').remove();
+  };
+
+  // deletes place card on click of delete button
+  deleteButton.addEventListener('click', deletePlace);
+
+  const picturePopup = () => {
     popupImageLarge.classList.add('popup_open_image');
     popupPhoto.src = placeImage.src;
     popupTitle.textContent = placeName.textContent;
   };
-
   //on click on an image in the place cards, a popup of the image will appear
   placeImage.addEventListener('click', picturePopup);
 
-  const deleteButton = placeElement.querySelector('.places__delete-button');
-  deleteButton.addEventListener('click', deletePlace);
+  return placeElement;
+};
 
-  //make cards appear online
-  placesList.prepend(placeElement);
+// accesses the place card template and places it in the DOM
+const addPlace = (place) => {
+  const placeElement = createPlaceTemplate(place);
    
+  //make cards appear online
+  placesList.prepend(placeElement); 
 };
 
 /* loops through the initialPlaces array to load each place card with the addPlace function */
 initialPlaces.forEach((place) => {
   addPlace(place);
-
 });
 
+ /*updates the name and title input field, closes the popup, and prevents the default response from browser */
+const formSubmitEdit = (evt) => {
+  evt.preventDefault(); //stops browser from submitting the form in the default way (refreshes whenever you submit)
+  profileName.textContent = newName.value;
+  profileTitle.textContent = newTitle.value;
+  closePopup();
+}
+
 //updates the title and image, closes the popup, and prevents the default response from browser
-function formSubmitAdd(evt) {
+const formSubmitAdd = (evt) => {
   evt.preventDefault(); //stops browser from submitting the form in the default way (refreshes whenever you submit)
   const place = {
     name: imageTitle.value,
@@ -128,16 +139,11 @@ function formSubmitAdd(evt) {
   }
   addPlace(place);
   closePopup();
-
 }
 
 /*when click the save button, runs the formSubmit function */
+popupForms[0].addEventListener('submit', formSubmitEdit);
+/*when click the save button, runs the formSubmit function */
 popupForms[1].addEventListener('submit', formSubmitAdd);
 
-/* Delete place card */
-
-/* removes the selected place */
- function deletePlace(evt) {
-  evt.target.closest('.places__item').remove();
- };
 
