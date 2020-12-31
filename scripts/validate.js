4285c34f431c22e4b70359caa4f05a993f55e749
kaithill <k.m.hiller@gmail.com>
2020-12-30 23:48:51 -0500

    remove validate.js - add settingsObject to FormValidtor.js

--- scripts/validate.js
+++
@@ -1,9 +0,0 @@
-
-// const settingsObject = {
-//   formSelector: ".form",
-//   inputSelector: ".popup__input",
-//   submitButton: ".popup__save-button",
-//   inactiveButtonClass: "popup__save-button_disabled",
-//   inputErrorClass: "popup__input-error"
-// }; 
-
