8c3d3f96b1f9e7a4f03b25b922b961ddb9dd7b6a
kaithill <k.m.hiller@gmail.com>
2021-01-03 12:40:23 -0500

    removed commented out, old code

--- scripts/script.js
+++ scripts/script.js
@@ -25,6 +25,7 @@ const newTitle = document.querySelector('.popup__input_text_title'); /*title wri
 const saveButton = document.querySelectorAll('.popup__save-button'); /* save button */
 const placesForm = document.querySelector('.popup__form-places');
 const profileForm = document.querySelector('.popup__form-profile');
+const form = document.querySelector('.form');
 
 /* places image popup */
 const popupImageLarge = document.querySelector('.popup_image-large');
@@ -74,8 +75,7 @@ profileExit.addEventListener('click', () => {
 // when clicks, the popup box opens 
 addPlaceButton.addEventListener('click', () => {
   openPopup(popupEditPlaces);
-  imageLink.value = "";
-  imageTitle.value = "";
+  resetForm(placesForm);
 });
 // when clicks, the add places popup box closes
 addPlacesExit.addEventListener('click', () => {
@@ -108,6 +108,10 @@ placesForm.addEventListener('submit', (evt) => {
   closePopup(popupEditPlaces);
 });
 
+const resetForm = (form) => {
+  form.reset();
+}
+
 // creates a new instance of a Card
 const renderElements = (place) => {
   const card = new Card(place, "#place-template");
@@ -132,69 +136,6 @@ closePopupFromOverlay();
 
 // allows the pre-existing cards to automatically load onload of site
 onload();
-
-/* PLACE CARD SECTION */
-
-// creates and returns a template for the place cards
-/*
-function createPlaceTemplate(place) {
- 
-  // place card template
-  const placeTemplate = document.querySelector('#place-template').content;
-  // clone content of place card template tag
-  const placeElement = placeTemplate.cloneNode(true);
-  const placeImage = placeElement.querySelector('.places__img');
-  const placeName = placeElement.querySelector('.places__name');
-
-  // using the text from the place card form fields, to create a new place card
-  placeImage.src = place.link;
-  placeImage.alt = place.name;
-  placeName.textContent = place.name;
-
-  // like button
-  const heartButton = placeElement.querySelector('.places__heart-button');
-  // When the heart button is clicked, a new class with a "liked" heart, appears
-  const likeButton = place => {
-    heartButton.classList.toggle('places__heart-button_active');
-  };
-  // toggles like button when clicked
-  heartButton.addEventListener('click', likeButton);
-
-  // image popup delete button
-  const deleteButton = placeElement.querySelector('.places__delete-button');
-
-  // removes the selected place card 
-  const deletePlace = (evt) => {
-    evt.target.closest('.places__item').remove();
-  };
-
-  // deletes place card on click of delete button
-  deleteButton.addEventListener('click', deletePlace);
-
-  // on click on an image in the place cards, a popup of the image will appear
-  placeImage.addEventListener('click', () => {
-    popupPhoto.src = placeImage.src;
-    popupTitle.textContent = placeName.textContent;
-    openPopup(popupImageLarge);
-  });
-
-  return placeElement;
-}
-*/
-// accesses the place card template and places it in the DOM
-// const addPlace = (place) => {
-//   const placeElement = createPlaceTemplate(place);
-   
-//   //make cards appear online
-//   placesList.prepend(placeElement); 
-// };
-
-// const onload = (place) => {
-//   addPlace(place);
-// };
-
-/* loops through the initialPlaces array to load each place card with the addPlace function */
-// initialPlaces.forEach(onload);
 
 
 export { initialPlaces }; 
